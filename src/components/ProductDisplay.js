01ba446dae79bba7acc365e39775a374a038a3dc
pengibot <pengibot@gmail.com>
2024-03-14 10:54:51 +0000

    some minor bug fixes for Checkout

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-// Define the ProductDisplay component
 const ProductDisplay = ({ product, onAddToCart }) => {
   let navigate = useNavigate();
 
